567871f886e56ac2574b09ce8c7da5f9613c48ae
Corey Osman <corey@nwops.io>
2021-12-13 16:52:31 -0800

    Update r10k-environments script

--- site-modules/profile/manifests/puppetserver.pp
+++ site-modules/profile/manifests/puppetserver.pp
@@ -1,5 +1,5 @@
 class profile::puppetserver {
-  file{'/etc/puppetlabs/r10k/r10k-environments.sh':
+  file{'/etc/puppetlabs/r10k/r10k-environments-git.sh':
     ensure => present,
     mode   => '0755',
     user   => 'root',
